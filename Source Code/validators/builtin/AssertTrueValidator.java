f6fc8935d4f3e51052145f7884efed285be6c2f4
thinhnguyenphuc <thinhnguyenphuc55@gmail.com>
2021-12-27 10:52:44 +0700

    Init assertValidator

---
+++ Source Code/validators/builtin/AssertTrueValidator.java
@@ -0,0 +1,17 @@
+package validators.builtin;
+
+public class AssertTrueValidator<T> extends BuiltInValidator<T> {
+    public AssertTrueValidator(boolean exitWhenFailed) {
+        super(exitWhenFailed);
+    }
+
+    @Override
+    protected void createDefaultFailedMessage() {
+
+    }
+
+    @Override
+    public boolean isValid(T t) {
+        return false;
+    }
+}
