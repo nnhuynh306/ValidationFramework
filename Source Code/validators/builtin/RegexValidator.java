67bd751085f5c5e8af6739a057d80fe20965d8cb
Thai Ngo <ngothanhphuongthai@gmail.com>
2021-12-29 23:02:19 +0700

    fix: use string instead of generic

--- Source Code/validators/builtin/RegexValidator.java
+++ Source Code/validators/builtin/RegexValidator.java
@@ -1,6 +1,9 @@
 package validators.builtin;
 
-public class RegexValidator<T> extends BuiltInValidator<T> {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexValidator<String> extends BuiltInValidator<String> {
     private final String regex;
 
     public RegexValidator(boolean exitWhenFailed, String regex) {
@@ -14,8 +17,9 @@ public class RegexValidator<T> extends BuiltInValidator<T> {
     }
 
     @Override
-    public boolean isValid(T t) {
-        if (t instanceof String) return ((String) t).matches(regex);
-        return false;
+    public boolean isValid(String str) {
+        Pattern pattern = Pattern.compile((java.lang.String) regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher((CharSequence) str);
+        return matcher.find();
     }
 }
