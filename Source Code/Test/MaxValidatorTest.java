6d795d072eb27c59baee58dedc189c57d151f183
Thai Ngo <ngothanhphuongthai@gmail.com>
2022-01-10 22:28:10 +0700

    test: min and max test

--- Source Code/Test/MaxValidatorTest.java
+++ Source Code/Test/MaxValidatorTest.java
@@ -2,15 +2,31 @@ package Test;
 
 import org.junit.Assert;
 import org.junit.Test;
-import util.comparator.Comparator;
-import util.comparator.IntegerComparator;
+import util.comparator.*;
 import validators.builtin.MaxValidator;
 
 public class MaxValidatorTest {
     @Test
-    public void maxIntValidatorTest(){
-        Comparator<Integer,Integer> comparator = new IntegerComparator();
-        MaxValidator maxValidator = new MaxValidator(comparator,5,true);
+    public void maxIntValidatorTest() {
+        Comparator<Integer, Integer> comparator = new IntegerComparator();
+        MaxValidator maxValidator = new MaxValidator(comparator, 5, true);
         Assert.assertTrue(maxValidator.isValid(3));
+        Assert.assertFalse(maxValidator.isValid(7));
+    }
+
+    @Test
+    public void maxDoubleValidatorTest() {
+        Comparator<Double, Double> comparator = new DoubleComparator();
+        MaxValidator maxValidator = new MaxValidator(comparator, 5.5, true);
+        Assert.assertTrue(maxValidator.isValid(5.5));
+        Assert.assertFalse(maxValidator.isValid(6.3));
+    }
+
+    @Test
+    public void maxLongValidatorTest() {
+        Comparator<Long, Long> comparator = new LongComparator();
+        MaxValidator maxValidator = new MaxValidator(comparator, Long.parseLong("102400000"), false);
+        Assert.assertTrue(maxValidator.isValid(Long.parseLong("102300000")));
+        Assert.assertFalse(maxValidator.isValid(Long.parseLong("102400000")));
     }
 }
--- Source Code/Test/MinValidatorTest.java
+++ Source Code/Test/MinValidatorTest.java
@@ -1,10 +1,32 @@
 package Test;
 
+import org.junit.Assert;
 import org.junit.Test;
+import util.comparator.*;
+import validators.builtin.MinValidator;
 
 public class MinValidatorTest {
     @Test
-    public void minTest(){
+    public void MinIntValidatorTest() {
+        Comparator<Integer, Integer> comparator = new IntegerComparator();
+        MinValidator MinValidator = new MinValidator(comparator, 5, true);
+        Assert.assertFalse(MinValidator.isValid(3));
+        Assert.assertTrue(MinValidator.isValid(7));
+    }
 
+    @Test
+    public void MinDoubleValidatorTest() {
+        Comparator<Double, Double> comparator = new DoubleComparator();
+        MinValidator MinValidator = new MinValidator(comparator, 5.5, true);
+        Assert.assertTrue(MinValidator.isValid(5.5));
+        Assert.assertFalse(MinValidator.isValid(5.3));
+    }
+
+    @Test
+    public void MinLongValidatorTest() {
+        Comparator<Long, Long> comparator = new LongComparator();
+        MinValidator MinValidator = new MinValidator(comparator, Long.parseLong("102400000"), false);
+        Assert.assertTrue(MinValidator.isValid(Long.parseLong("102500000")));
+        Assert.assertFalse(MinValidator.isValid(Long.parseLong("102400000")));
     }
 }
