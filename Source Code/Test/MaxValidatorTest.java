6c86c431422236fbc7364e4ea53e710cfd71c709
thinhnguyenphuc <thinhnguyenphuc55@gmail.com>
2022-01-10 22:14:45 +0700

    Add max int validator test

--- Source Code/Test/MaxValidatorTest.java
+++ Source Code/Test/MaxValidatorTest.java
@@ -1,10 +1,16 @@
 package Test;
 
+import org.junit.Assert;
 import org.junit.Test;
+import util.comparator.Comparator;
+import util.comparator.IntegerComparator;
+import validators.builtin.MaxValidator;
 
 public class MaxValidatorTest {
     @Test
-    public void maxValidatorTest(){
-
+    public void maxIntValidatorTest(){
+        Comparator<Integer,Integer> comparator = new IntegerComparator();
+        MaxValidator maxValidator = new MaxValidator(comparator,5,true);
+        Assert.assertTrue(maxValidator.isValid(3));
     }
 }
